d9d38ba8579f4c32e5d1d0d931f9103b765205fb
Daniel Lewis <daniel@levelsetlabs.com>
2013-08-28 00:04:05 -0700

    just for now

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Noisepatch::Application.config.secret_token = '696276c579ee79a89d8043128138d25f3028b7fe781b4067b1e24e3284e1002c7b5ef502d46e7ea184786c40233ca3c3cbf5f4406609ae668c75b4ea8955dc72'
